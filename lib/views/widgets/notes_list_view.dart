ffd4b1d160fa8dcde944398060d30484dc4555ef
raghadhawat <hawatraghad123@gmail.com>
2023-11-24 14:26:59 +0300

    show model buttom sheet

--- lib/views/widgets/notes_list_view.dart
+++ lib/views/widgets/notes_list_view.dart
@@ -5,15 +5,21 @@ class NotesListView extends StatelessWidget {
   const NotesListView({super.key});
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (context, index) {
-        return const Padding(
-          padding: EdgeInsets.symmetric(
-            vertical: 4,
-          ),
-          child: NotesItem(),
-        );
-      },
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        vertical: 16,
+      ),
+      child: ListView.builder(
+        padding: EdgeInsets.zero,
+        itemBuilder: (context, index) {
+          return const Padding(
+            padding: EdgeInsets.symmetric(
+              vertical: 4,
+            ),
+            child: NotesItem(),
+          );
+        },
+      ),
     );
   }
 }
