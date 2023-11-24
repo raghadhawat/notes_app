f3d212ee7acebb6d1d26b7ddf8ee2dc48cd2aca0
raghadhawat <hawatraghad123@gmail.com>
2023-11-24 17:14:30 +0300

    create note view

--- lib/views/widgets/custom_note_item.dart
+++ lib/views/widgets/custom_note_item.dart
@@ -1,65 +1,76 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/views/edit_note_view.dart';
 
 class NotesItem extends StatelessWidget {
   const NotesItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(
-        top: 24,
-        bottom: 24,
-        left: 16,
-      ),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16),
-        color: const Color(0xffFFCC80),
-      ),
-      child: Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
-        ListTile(
-          title: const Text(
-            'Flutter Tips',
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 26,
-            ),
-          ),
-          subtitle: Padding(
-            padding: const EdgeInsets.only(
-              top: 16,
-              bottom: 16,
-            ),
-            child: Text(
-              'Build your career with raghad hawat',
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) {
+            return const EditNoteView();
+          }),
+        );
+      },
+      child: Container(
+        padding: const EdgeInsets.only(
+          top: 24,
+          bottom: 24,
+          left: 16,
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          color: const Color(0xffFFCC80),
+        ),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
+          ListTile(
+            title: const Text(
+              'Flutter Tips',
               style: TextStyle(
-                color: Colors.black.withOpacity(.5),
-                fontSize: 18,
+                color: Colors.black,
+                fontSize: 26,
+              ),
+            ),
+            subtitle: Padding(
+              padding: const EdgeInsets.only(
+                top: 16,
+                bottom: 16,
+              ),
+              child: Text(
+                'Build your career with raghad hawat',
+                style: TextStyle(
+                  color: Colors.black.withOpacity(.5),
+                  fontSize: 18,
+                ),
+              ),
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                FontAwesomeIcons.trash,
+                color: Colors.black,
+                size: 24,
               ),
             ),
           ),
-          trailing: IconButton(
-            onPressed: () {},
-            icon: const Icon(
-              FontAwesomeIcons.trash,
-              color: Colors.black,
-              size: 24,
+          Padding(
+            padding: const EdgeInsets.only(
+              right: 24,
+            ),
+            child: Text(
+              'May 21, 2022',
+              style: TextStyle(
+                color: Colors.black.withOpacity(.4),
+                fontSize: 16,
+              ),
             ),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(
-            right: 24,
-          ),
-          child: Text(
-            'May 21, 2022',
-            style: TextStyle(
-              color: Colors.black.withOpacity(.4),
-              fontSize: 16,
-            ),
-          ),
-        ),
-      ]),
+        ]),
+      ),
     );
   }
 }
